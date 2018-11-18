dcaedd0b23e1bf44e17a6ebd6ada92cfb37f5356
rafapitzer <rafa_thadeupitzer@hotmail.com>
2018-11-18 14:06:24 -0200

    incluido classes

---
+++ src/entities/OrderItem.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class OrderItem {
+	
+	private Integer quantity;
+	private Double price;
+	
+	private Product product;
+	
+	public OrderItem() {
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	public Double subTotal() {
+		return quantity * price;
+	}
+}
